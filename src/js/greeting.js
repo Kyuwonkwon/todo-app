fc8c47311e0948e09867217061dbe7e18b09acde
QWON <kkwon920@naver.com>
2022-11-07 01:36:57 +0900

    input design change
    add transparency

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -1,8 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const lowerBar = document.querySelector(".lower-bar");
+const main = document.querySelector("main");
 
 const HIDDEN_CLASSNAME = "hidden";
+const TRANSPARENCY_CLASSNAME = "transparency";
 const USERNAME_KEY = "username";
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -19,9 +22,13 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `안녕하세요 ${username}님`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  main.classList.remove(TRANSPARENCY_CLASSNAME);
+  lowerBar.classList.remove(TRANSPARENCY_CLASSNAME);
 }
 
 if (savedUsername === null) {
+  main.classList.add(TRANSPARENCY_CLASSNAME);
+  lowerBar.classList.add(TRANSPARENCY_CLASSNAME);
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
